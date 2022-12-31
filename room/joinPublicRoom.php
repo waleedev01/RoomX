d60229b5c0e0e99cc802d708c2ed4d1630c8a879
Abul Chowdhury <abulchwdhury@gmail.com>
2022-12-31 15:37:03 +0000

    update css path

--- room/joinPublicRoom.php
+++ room/joinPublicRoom.php
@@ -10,7 +10,7 @@ include '../session/session.php';
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="tableSearch.js"></script>
-<link rel="stylesheet" href="table.css">
+<link rel="stylesheet" href="tables.css">
 <head>
 
 <body>
